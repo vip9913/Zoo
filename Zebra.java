08b6a3f0312df25f437783da32c8b05d3b8486b5
vip9913 <alex_velig@mail.ru>
2019-01-02 14:36:50 +0300

    Zebra

---
+++ Zebra.java
@@ -0,0 +1,7 @@
+public class Zebra
+{
+	public Zebra()
+	{
+		Sistem.out.priteln("Sara");
+	}
+}
--- Zoo.java
+++ Zoo.java
@@ -2,4 +2,5 @@ public class Zoo
 {
 	public static void main(String ...args[])
 	System.out.printeln("Welcome to Zoo");
+	Zebra zebra= new Zebra();
 }
